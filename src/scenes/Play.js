78fe62b89f21ed6cb6f45aa263d4eba2cef45ab8
Darren Yang <darrenyang@Darrens-MacBook-Pro.local>
2021-07-02 01:38:16 -0700

    add starfield

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,9 +3,17 @@ class Play extends Phaser.Scene {
        super("playScene");
     }
 
+    preload() {
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
     create(){
         this.add.text(20, 20, "Rocket Patrol Play");
 
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0,0);
+        
         this.add.rectangle(0, borderUISize + borderPadding,
             game.config.width,
             borderUISize * 2, 0x00FF00).setOrigin(0,0);
@@ -14,6 +22,10 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0XFFFFFF).setOrigin(0,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, borderUISize, 0XFFFFFF).setOrigin(0,0);
 
+    }
+
+    update(){
+        this.starfield.tilePositionX -= 4;
     }
 
 }
