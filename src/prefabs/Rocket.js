d84077f6f5f56b6d77ee37a116fd496f1405e585
Darren Yang <yyang199@ucsc.edu>
2021-07-02 19:16:48 -0700

    add sounds

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,12 +1,12 @@
-// Rocket prefab
+
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
 
-        scene.add.existing(this);   // add to existing, displayList, updateList
-        this.isFiring = false;      // track rocket's firing status
-        this.moveSpeed = 2;         // pixels per frame
-        this.sfxRocket = scene.sound.add('sfx_rocket')  // add rocket sfx
+        scene.add.existing(this);   
+        this.isFiring = false;      
+        this.moveSpeed = 2;         
+        this.sfxRocket = scene.sound.add('sfx_rocket') 
     }
 
     update() {
