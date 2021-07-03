f2f78a045a3c0d4c8c05ca36d4fb41959c7e3938
Darren Yang <yyang199@ucsc.edu>
2021-07-02 19:18:05 -0700

    add score

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -10,7 +10,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
     }
 
     update() {
-        // left/right movement
+        
         if(!this.isFiring) {
             if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
                 this.x -= this.moveSpeed;
@@ -18,22 +18,22 @@ class Rocket extends Phaser.GameObjects.Sprite {
                 this.x += this.moveSpeed;
             }
         }
-        // fire button
+        
         if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
             this.sfxRocket.play();
         }
-        // if fired, move up
+        
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
             this.y -= this.moveSpeed;
         }
-        // reset on miss
+        
         if(this.y <= borderUISize * 3 + borderPadding) {
             this.reset();
         }
     }
 
-    // reset rocket to "ground"
+    
     reset() {
         this.isFiring = false;
         this.y = game.config.height - borderUISize - borderPadding;
