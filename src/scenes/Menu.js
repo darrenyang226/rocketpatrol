5a754565cef6e987247a9f8d8adb3dd026485867
Darren Yang <yyang199@ucsc.edu>
2021-07-02 19:18:53 -0700

    add menu text

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,14 +4,14 @@ class Menu extends Phaser.Scene {
   }
 
   preload() {
-      // load audio
+      
       this.load.audio('sfx_select', './assets/blip_select12.wav');
       this.load.audio('sfx_explosion', './assets/explosion38.wav');
       this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
   }
 
   create() {
-      // menu text configuration
+     
       let menuConfig = {
           fontFamily: 'Courier',
           fontSize: '28px',
@@ -25,21 +25,21 @@ class Menu extends Phaser.Scene {
           fixedWidth: 0
       }
       
-      // show menu text
+      
       this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
       this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
       menuConfig.backgroundColor = '#00FF00';
       menuConfig.color = '#000';
       this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
 
-      // define keys
+      
       keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
       keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
   }
 
   update() {
       if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
-        // Novice mode
+        
         game.settings = {
           spaceshipSpeed: 3,
           gameTimer: 60000    
@@ -48,7 +48,7 @@ class Menu extends Phaser.Scene {
         this.scene.start("playScene");    
       }
       if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
-        // Expert mode
+        
         game.settings = {
           spaceshipSpeed: 4,
           gameTimer: 45000    
